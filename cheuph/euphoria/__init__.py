bc203aaff171438b250a84f052c922e87cdbef83
Joscha <joscha@plugh.de>
2019-05-16 11:12:39 +0000

    Create palette from config and add default config

--- cheuph/euphoria/__init__.py
+++ cheuph/euphoria/__init__.py
@@ -1,8 +1,8 @@
 from typing import List
 
-from .palette import *
 from .single_room_application import *
+from .util import *
 
 __all__: List[str] = []
-__all__ += palette.__all__
 __all__ += single_room_application.__all__
+__all__ += util.__all__
